b0ba51b43d9da6a3b32bfc54defd830e21100914
Arjun singh gurjar <115486632+Arjun09091999@users.noreply.github.com>
2023-05-19 16:19:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function getFormvalue() {
 const form = document.getElementById("form1");
 const firstName = form.fname.value;
 const lastName = form.lname.value;
-alert("First Name: " + firstName + "\nLast Name: " + lastName);
+alert( firstName + " " + lastName);
 }
